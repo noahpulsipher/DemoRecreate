8e245769b099c26ddca56eb02d0cf815a5cce29e
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-10-10 11:49:25 -0600

    Created another app that does the same as FirstAfternoonDemo, changes button color to random color when clicked, for practice

--- DemoRecreate/ViewController.swift
+++ DemoRecreate/ViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var firstButton: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        if (firstButton.backgroundColor == .orange)
+        {
+            firstButton.backgroundColor = .black
+        }
+        else
+        {
+            firstButton.backgroundColor = .orange
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor: UIColor
+        let redValue: CGFloat = CGFloat (Double(arc4random_uniform(256)) / 255.00)
+        let greenValue: CGFloat = CGFloat (Double(arc4random_uniform(256)) / 255.00)
+        let blueValue: CGFloat = CGFloat (Double(arc4random_uniform(256)) / 255.00)
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
+        return newColor
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
