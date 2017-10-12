c602cf3ec608cf64f5f27ee8abfc5f6ea2db07be
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-10-12 12:33:51 -0600

    Added a text box, switch, and slider and when you slide the slider it changes everything to a random color, when the switch is pushed it sets everything to black.

--- DemoRecreate/Controller/ViewController.swift
+++ DemoRecreate/Controller/ViewController.swift
@@ -11,6 +11,31 @@ import UIKit
 class ViewController: UIViewController
 {
 
+    @IBOutlet weak var Slider: UISlider!
+    
+    @IBOutlet weak var firstSwitch: UISwitch!
+    
+    @IBOutlet weak var textBox: UITextField!
+    
+    @IBAction func switchMethod(_ sender: UISwitch)
+    {
+        view.backgroundColor = .black
+        firstButton.setTitleColor(.black, for: .normal)
+        firstButton.backgroundColor = .black
+        Slider.backgroundColor = .black
+        textBox.backgroundColor = .black
+        textBox.textColor = .black
+    }
+    @IBAction func sliderMethod(_ sender: UISlider)
+    {
+        view.backgroundColor = createRandomColor()
+        firstButton.setTitleColor(createRandomColor(), for: .normal)
+        firstButton.backgroundColor = createRandomColor()
+        Slider.backgroundColor = createRandomColor()
+        textBox.backgroundColor = createRandomColor()
+        textBox.textColor = createRandomColor()
+    }
+    
     @IBOutlet weak var firstButton: UIButton!
     
     override func viewDidLoad()
