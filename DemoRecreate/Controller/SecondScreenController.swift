b6a00d3b2789aebaacdacd40c8abfe2597413ec7
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-10-12 13:11:07 -0600

    Made a new screen to storyboard and a controller for it.

---
+++ DemoRecreate/Controller/SecondScreenController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondScreenController.swift
+//  DemoRecreate
+//
+//  Created by Pulsipher, Noah on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class SecondScreenController: UIViewController
+{
+    
+    
+}
